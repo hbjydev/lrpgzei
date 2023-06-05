98762d698e217998fbb668c8c45f84271f310b80
Hayden Young <hayden@kuraudo.io>
2023-06-05 18:04:18 +0100

    feat: add mod metadata files

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "[LRPG] ZEI";
+picture = "data/zeus.paa";
+actionName = "Website";
+action = "https://discord.gg/5VTVhZMjSa";
+tooltip = "ZEI 1.2";
+description = "LRPG's fork of Zeus and Eden Interiors";
+overview = "ZEI adds interiors to buildings.";
+author = "[LRPG] Young";
